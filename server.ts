54b549a777725f718926408adc24e6fc797de147
supergrahn <tom_espen@live.com>
2026-03-26 09:14:34 +0100

    fix: use ES import for ptyMap in server.ts instead of require()

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { createServer } from 'http'
 import { parse } from 'url'
 import next from 'next'
 import { WebSocketServer } from 'ws'
-import { handleWebSocket } from './lib/session-manager'
+import { handleWebSocket, ptyMap } from './lib/session-manager'
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev, turbo: dev })
@@ -29,7 +29,6 @@ app.prepare().then(() => {
   })
 
   // Kill all PTYs on shutdown
-  const { ptyMap } = require('./lib/session-manager')
   const shutdown = () => {
     for (const proc of (ptyMap as Map<string, any>).values()) {
       try { proc.kill() } catch {}
